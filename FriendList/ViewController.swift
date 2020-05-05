2b97f30c53d659523d27fc935277fcfd1bdb218e
Nigostarr <wlckdgus5833@naver.com>
2020-05-05 13:12:23 +0900

    Initial Commit

---
+++ FriendList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendList
+//
+//  Created by Ji Chang Hyun on 2020/05/05.
+//  Copyright © 2020 Nigostarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
